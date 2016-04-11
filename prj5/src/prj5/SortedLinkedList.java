30c127373484f392663cf09d8162831f04f30a61
Mr. Lawrence P-S <lawpeczkowski@gmail.com>
2016-04-11 19:14:21 -0400

    Added some classes. Started the SortedLinkedList

---
+++ prj5/src/prj5/SortedLinkedList.java
@@ -0,0 +1,25 @@
+package prj5;
+
+import java.util.LinkedList;
+
+public class SortedLinkedList<Glyph> extends LinkedList<Glyph> {
+    //fields
+    private Node<Glyph> head;
+    private Node<Glyph> tail;
+    private int size;
+    private Comparator comp;
+    
+    public SortedLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+    
+    
+    public boolean add(Glyph entry) {
+        //TODO 
+        return false;
+    }
+    
+    
+}
