a254c65d3802b4b33047ce264f893e27ef121f72
Mr. Lawrence P-S <lawpeczkowski@gmail.com>
2016-04-11 19:33:03 -0400

    Added blank compare method and added methods to the SortedLinkedList

--- prj5/src/prj5/GlyphComparator.java
+++ prj5/src/prj5/GlyphComparator.java
@@ -2,5 +2,7 @@ package prj5;
 
 public class GlyphComparator<Glyph> {
 
-    
+    public int compare(Glyph g1, Glyph g2) {
+        return 0;
+    }
 }
--- prj5/src/prj5/SortedLinkedList.java
+++ prj5/src/prj5/SortedLinkedList.java
@@ -7,19 +7,89 @@ public class SortedLinkedList<Glyph> extends LinkedList<Glyph> {
     private Node<Glyph> head;
     private Node<Glyph> tail;
     private int size;
-    private GlyphComparator comp;
+    private GlyphComparator<Glyph> comp;
     
     public SortedLinkedList() {
         head = null;
         tail = null;
         size = 0;
+        comp = new GlyphComparator<Glyph>();
     }
     
+    /**
+     * Returns an element in a specified position in the list.
+     * @param index if the number.
+     * @return Returns the element in the specified position.
+     * @throws IndexOutOfBoundsException if the index is out of range.
+     */
+    @Override
+    public Glyph get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        else {
+            Node<Glyph> temp = head;
+            int counter = 0;
+            while (counter != index) {
+                temp = temp.getNext();
+                counter++;
+            }
+            return temp.getData();
+        }
+    }
+
+    /**
+     * Gets the number of elements in the list.
+     * @return Returns the number of elements in the list.
+     */
+    @Override
+    public int size() {
+        return size;
+    }
     
+    /**
+     * Adds a new node to the sorted list. 
+     * @param entry is the data being stored.
+     * @return Returns true if the entry was successfully added to the list.
+     * @throws NullPointerException if entry is equal to null;
+     */
+    @Override
     public boolean add(Glyph entry) {
-        //TODO 
-        return false;
+        if (head == null) {
+            head = new Node<Glyph>(entry);
+            size++;
+            return true;
+        }
+        else if (comp.compare(entry, head.getData()) < 0) {
+            Node<Glyph> next = head;
+            head = new Node<Glyph>(entry);
+            head.setNext(next);
+            size++;
+            return true;
+        }
+        else {
+            Node<Glyph> curr = head.getNext();
+            Node<Glyph> prev = head;
+            if (curr != null) {
+                while (comp.compare(entry, curr.getData()) >= 0) {
+                    curr = curr.getNext();
+                    prev = prev.getNext();
+                    if (curr == null)
+                    {
+                        break;
+                    }
+                }
+                Node<Glyph> newNode = new Node<Glyph>(entry);
+
+                newNode.setNext(curr);
+                prev.setNext(newNode); 
+                
+            }
+            else {
+                prev.setNext(new Node<Glyph>(entry));
+            }
+            size++;
+            return true;
+        }
     }
-    
-    
 }
